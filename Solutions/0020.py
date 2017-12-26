fccc8de8edcadb89d4408a926e9acc02743cba5d
Zachary Jacobi <zach@alertlabs.com>
2017-12-26 17:15:40 -0500

    Solved problem 20

---
+++ Solutions/0020.py
@@ -0,0 +1,15 @@
+"""
+# PROBLEM 20
+n! means n x (n - 1) x ... x 3 x 2 x 1
+
+For example, 10! = 10 x 9 x ... x 3 x 2 x 1 = 3628800,
+and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+Find the sum of the digits in the number 100!
+"""
+
+from math import factorial
+
+# Python is a good friendo and gives us arbitrary high integers. Thanks Python!
+
+print(sum([int(num) for num in str(factorial(100))]))
