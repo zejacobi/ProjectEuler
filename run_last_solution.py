f1bacf8f4eb0e7259b3c19eee53a1ce2392480d9
Zachary Jacobi <zach@alertlabs.com>
2018-02-27 23:56:38 -0500

    Changes how the `run_last_solution` file works so that it filters out all auto-gen'd python files.

--- run_last_solution.py
+++ run_last_solution.py
@@ -2,10 +2,9 @@ from glob import glob
 from os import path
 
 options = sorted([path for path in glob('.' + path.sep + 'Solutions' + path.sep + '*')
-                 if 'init' not in path])
+                 if '__' not in path])
 
 last = options[-1]
 
 last_name = last.split(path.sep)[-1].split('.')[0]
-
 __import__('Solutions.' + last_name)
