998208196945039c0976e0ae5158d8fc04b83b95
Zachary Jacobi <zach@alertlabs.com>
2018-06-09 23:46:22 -0400

    Switched gears on 47 and solved it with trial division. I don't _love_ my solution, but it gets the job done.

--- Solutions/0047.py
+++ Solutions/0047.py
@@ -21,12 +21,65 @@ What is the first of these numbers?
 # (At least python doesn't hate the unicode ² in the problem; it's previously thrown a fit about
 # unicode characters and proper encodings)
 
+# Well, I think the thing I hate is that they're slow. Now that I've realized I can just do trial
+# division, this doesn't seem as bad. Although it was very slow before I hit on a memoization
+# scheme to square root one of the n's inherent in trial division.
 
-# Okay I think I just have to bite the bullet and implement a factoring algorithm from Wikipedia.
-# I'm going to use the "Rational sieve"; it's apparently a bit slower than the "General Field
-# Sieve", but simpler, which is good, because implementing algorithms from Wikipedia is always such
-# an absolute shit-show. I don't know how they make them so difficult to understand, but they do.
+# The result is still inelegant, but it's better than trying to understand one of Wikipeida's
+# "nicer" factorization algorithms, which are _dreadfully_ explained.
 
-# (I could find some better explanation, but if I don't suffer while solving these, it feels like I
-# cheated and takes away half the fun. From that lens, Wikipedia's terrible pseudocode and
-# annoyingly minimalistic conventions are a blessing in disguise.)
+from math import sqrt
+
+from Lib.Helpers import list_big_primes
+
+primes = list_big_primes(100000)  # we can now search up to the square of the 100,000th prime
+
+three_ago = False
+two_ago = False
+one_ago = False
+
+
+past_factors = {}
+
+for i in range(2 * 3 * 5, primes[-1] ** 2):
+    if i in primes:
+        one_ago = False
+        two_ago = False
+        three_ago = False
+        continue
+
+    factors = []
+    for potential_factor in primes:
+        if len(factors) > 4:
+            break
+        if potential_factor > sqrt(i):  # memoization makes the n sqrt'd, which is v. sweet.
+            break
+        if i % potential_factor == 0 and potential_factor not in factors:
+            factors.append(potential_factor)
+            other_factor = int(i / potential_factor)
+
+            # at this point, the number is either prime, which means we can append it directly to
+            # our factors and restrict our search to sqrt(i) and below, or it's composite, in which
+            # case we already know all of its factors.
+            if other_factor in primes and other_factor not in factors:
+                factors.append(other_factor)
+
+            elif past_factors.get(other_factor, None):
+                for extra_factor in past_factors[other_factor]:
+                    if extra_factor not in factors:
+                        factors.append(extra_factor)
+
+    past_factors[i] = factors
+
+    if len(factors) == 4:
+        if one_ago and two_ago and three_ago:
+            print(three_ago)
+            exit()
+        else:
+            three_ago = two_ago
+            two_ago = one_ago
+            one_ago = i
+    else:
+        one_ago = False
+        two_ago = False
+        three_ago = False
